ea6f787c098bc353aba3c970b68470f8114f7d32
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-12 02:09:39 +0800

    chore: update MockitoBean import statements in controller test files

--- backend/src/test/java/com/glancy/backend/controller/ContactControllerTest.java
+++ backend/src/test/java/com/glancy/backend/controller/ContactControllerTest.java
@@ -12,9 +12,9 @@ import com.glancy.backend.service.ContactService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ContactController.class)
--- backend/src/test/java/com/glancy/backend/controller/WordControllerTest.java
+++ backend/src/test/java/com/glancy/backend/controller/WordControllerTest.java
@@ -16,9 +16,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(WordController.class)
