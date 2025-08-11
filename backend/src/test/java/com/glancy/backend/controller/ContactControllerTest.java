65fdfb99595398a250cc1727ecc0ea0f02943a22
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-12 01:40:19 +0800

    fix: replace MockitoBean with MockBean in tests (#13)

--- backend/src/test/java/com/glancy/backend/controller/ContactControllerTest.java
+++ backend/src/test/java/com/glancy/backend/controller/ContactControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ContactController.class)
@@ -24,10 +24,10 @@ class ContactControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private ContactService contactService;
 
-    @MockitoBean
+    @MockBean
     private com.glancy.backend.service.UserService userService;
 
     @Autowired
