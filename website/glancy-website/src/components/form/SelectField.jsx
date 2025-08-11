91d18a6e6c8b8e464d3d3ecd883608c8c2fe5b08
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-12 02:06:45 +0800

    feat(form): add reusable select components (#19)

---
+++ website/glancy-website/src/components/form/SelectField.jsx
@@ -0,0 +1,31 @@
+import FormRow from './FormRow.jsx'
+import styles from './Form.module.css'
+
+function SelectField({ label, id, value, onChange, options = [], className = '', ...props }) {
+  const cls = [styles.select, className].filter(Boolean).join(' ')
+  const handleChange = (e) => {
+    onChange && onChange(e.target.value)
+  }
+  const selectEl = (
+    <select id={id} className={cls} value={value} onChange={handleChange} {...props}>
+      {options.map((opt) => {
+        const option = typeof opt === 'string' ? { value: opt, label: opt } : opt
+        return (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        )
+      })}
+    </select>
+  )
+
+  return label ? (
+    <FormRow label={label} id={id}>
+      {selectEl}
+    </FormRow>
+  ) : (
+    selectEl
+  )
+}
+
+export default SelectField
