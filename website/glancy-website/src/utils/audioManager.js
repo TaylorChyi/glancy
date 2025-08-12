f21d417ce049289c3d82bb0994fa6c283931f49f
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-12 21:41:18 +0800

    feat: add tts player interaction

---
+++ website/glancy-website/src/utils/audioManager.js
@@ -0,0 +1,35 @@
+/**
+ * Global audio manager ensuring only one <audio> element plays at a time.
+ * It pauses any previously playing audio before starting a new one.
+ */
+class AudioManager {
+  constructor() {
+    this.current = null
+  }
+
+  /**
+   * Start playback on the provided element, stopping any prior audio.
+   * @param {HTMLAudioElement} audio
+   */
+  async play(audio) {
+    if (this.current && this.current !== audio) {
+      this.current.pause()
+    }
+    this.current = audio
+    return audio.play()
+  }
+
+  /**
+   * Stop playback if the element is the current one.
+   * @param {HTMLAudioElement} audio
+   */
+  stop(audio) {
+    if (!audio) return
+    audio.pause()
+    if (this.current === audio) {
+      this.current = null
+    }
+  }
+}
+
+export const audioManager = new AudioManager()
