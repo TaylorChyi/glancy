62892d80e07a6e54b0adedb74911236c5deee7d6
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-26 01:54:34 +0800

    clean code

--- backend/src/test/java/com/glancy/backend/controller/LocaleControllerTest.java
+++ backend/src/test/java/com/glancy/backend/controller/LocaleControllerTest.java
@@ -8,8 +8,8 @@ import java.util.Locale;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LocaleController.class)
--- backend/src/test/java/com/glancy/backend/controller/UserPreferenceControllerTest.java
+++ backend/src/test/java/com/glancy/backend/controller/UserPreferenceControllerTest.java
@@ -14,9 +14,9 @@ import com.glancy.backend.service.UserPreferenceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserPreferenceController.class)
