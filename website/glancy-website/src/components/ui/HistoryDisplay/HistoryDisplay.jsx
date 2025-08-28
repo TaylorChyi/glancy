85f014d79da4b036714a14e77a749b8a9b54db28
Tianyue Qi <taylor_chyi@icloud.com>
2025-08-28 13:31:47 +0800

    feat: virtualize history list (#186)

--- website/glancy-website/src/components/ui/HistoryDisplay/HistoryDisplay.jsx
+++ website/glancy-website/src/components/ui/HistoryDisplay/HistoryDisplay.jsx
@@ -1,22 +1,49 @@
-import { useHistory } from '@/context'
-import styles from './HistoryDisplay.module.css'
+import { forwardRef } from "react";
+import { FixedSizeList as List } from "react-window";
+import { useHistory, useLanguage } from "@/context";
+import styles from "./HistoryDisplay.module.css";
+
+const ITEM_HEIGHT = 32;
+
+const InnerElement = forwardRef(function InnerElement({ style, ...rest }, ref) {
+  return (
+    <ul
+      ref={ref}
+      style={style}
+      className={styles["history-grid-display"]}
+      {...rest}
+    />
+  );
+});
+
+const Row = ({ data, index, style }) => <li style={style}>{data[index]}</li>;
 
 function HistoryDisplay() {
-  const { history } = useHistory()
+  const { history } = useHistory();
+  const { t } = useLanguage();
+
   if (!history.length) {
     return (
       <div className="display-content">
-        <div className="display-term">No history</div>
+        <div className="display-term">{t.noHistory}</div>
       </div>
-    )
+    );
   }
+
+  const height = Math.min(400, history.length * ITEM_HEIGHT);
+
   return (
-    <ul className={styles['history-grid-display']}>
-      {history.map((h, i) => (
-        <li key={i}>{h}</li>
-      ))}
-    </ul>
-  )
+    <List
+      height={height}
+      itemCount={history.length}
+      itemSize={ITEM_HEIGHT}
+      width="100%"
+      itemData={history}
+      innerElementType={InnerElement}
+    >
+      {Row}
+    </List>
+  );
 }
 
-export default HistoryDisplay
+export default HistoryDisplay;
