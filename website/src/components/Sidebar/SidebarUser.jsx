62ae94eae9614dad6e0241a7145556721b703af7
Tianyue Qi <taylor_chyi@icloud.com>
2025-09-09 00:45:27 +0800

    refactor: centralize ICP link in sidebar (#212)

--- website/src/components/Sidebar/SidebarUser.jsx
+++ website/src/components/Sidebar/SidebarUser.jsx
@@ -1,12 +1,14 @@
-import { UserMenu } from '@/components/Header'
-import styles from './Sidebar.module.css'
+import { UserMenu } from "@/components/Header";
+import ICP from "@/components/ui/ICP";
+import styles from "./Sidebar.module.css";
 
 function SidebarUser() {
   return (
-    <div className={styles['sidebar-user']}>
+    <div className={styles["sidebar-user"]}>
       <UserMenu size={32} showName />
+      <ICP className={styles["sidebar-icp"]} />
     </div>
-  )
+  );
 }
 
-export default SidebarUser
+export default SidebarUser;
--- website/src/components/ui/ICP/ICP.jsx
+++ website/src/components/ui/ICP/ICP.jsx
@@ -1,15 +1,16 @@
-import { ICP_INFO } from '@/config/icp.js'
-import styles from './ICP.module.css'
+import { ICP_INFO } from "@/config/icp.js";
+import styles from "./ICP.module.css";
 
-function ICP() {
-  const { link, text } = ICP_INFO
+function ICP({ className }) {
+  const { link, text } = ICP_INFO;
+  const classes = [styles.icp, className].filter(Boolean).join(" ");
   return (
-    <div className={styles.icp}>
+    <div className={classes}>
       <a href={link} target="_blank" rel="noopener">
         {text}
       </a>
     </div>
-  )
+  );
 }
 
-export default ICP
+export default ICP;
