1e2718f2d72ff1cb55e46a0dbc5168cf7a66077f
Tianyue Qi <taylor_chyi@icloud.com>
2025-09-10 02:14:29 +0800

    Revert "feat(ui): add glass panel component (#218)"
    This reverts commit 311dfb7565498efd2698272f0857d75081297586.

--- website/src/components/ui/ListItem/ListItem.jsx
+++ website/src/components/ui/ListItem/ListItem.jsx
@@ -1,26 +1,13 @@
-import React from "react";
-import GlassPanel from "../GlassPanel";
-import styles from "./ListItem.module.css";
+import React from 'react'
+import styles from './ListItem.module.css'
 
-function ListItem({
-  text,
-  onClick,
-  actions,
-  className = "",
-  textClassName = "",
-}) {
+function ListItem({ text, onClick, actions, className = '', textClassName = '' }) {
   return (
-    <GlassPanel
-      as="li"
-      className={[styles.item, className].filter(Boolean).join(" ")}
-      onClick={onClick}
-    >
-      <span className={[styles.text, textClassName].filter(Boolean).join(" ")}>
-        {text}
-      </span>
+    <li className={[styles.item, className].filter(Boolean).join(' ')} onClick={onClick}>
+      <span className={[styles.text, textClassName].filter(Boolean).join(' ')}>{text}</span>
       {actions ? <div className={styles.actions}>{actions}</div> : null}
-    </GlassPanel>
-  );
+    </li>
+  )
 }
 
-export default ListItem;
+export default ListItem
