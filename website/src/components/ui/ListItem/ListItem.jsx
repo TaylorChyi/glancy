311dfb7565498efd2698272f0857d75081297586
Tianyue Qi <taylor_chyi@icloud.com>
2025-09-10 02:12:28 +0800

    feat(ui): add glass panel component (#218)

--- website/src/components/ui/ListItem/ListItem.jsx
+++ website/src/components/ui/ListItem/ListItem.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import styles from './ListItem.module.css'
+import React from "react";
+import GlassPanel from "../GlassPanel";
+import styles from "./ListItem.module.css";
 
-function ListItem({ text, onClick, actions, className = '', textClassName = '' }) {
+function ListItem({
+  text,
+  onClick,
+  actions,
+  className = "",
+  textClassName = "",
+}) {
   return (
-    <li className={[styles.item, className].filter(Boolean).join(' ')} onClick={onClick}>
-      <span className={[styles.text, textClassName].filter(Boolean).join(' ')}>{text}</span>
+    <GlassPanel
+      as="li"
+      className={[styles.item, className].filter(Boolean).join(" ")}
+      onClick={onClick}
+    >
+      <span className={[styles.text, textClassName].filter(Boolean).join(" ")}>
+        {text}
+      </span>
       {actions ? <div className={styles.actions}>{actions}</div> : null}
-    </li>
-  )
+    </GlassPanel>
+  );
 }
 
-export default ListItem
+export default ListItem;
