530b09285e7c0db05333f87fa0af1c6816391f8d
Tianyue Qi <taylor_chyi@icloud.com>
2025-09-17 20:01:14 +0800

    Enhance user menu modals layering and controls

--- website/src/components/modals/BaseModal.jsx
+++ website/src/components/modals/BaseModal.jsx
@@ -1,13 +1,30 @@
-import Modal from './Modal.jsx'
+import PropTypes from "prop-types";
+import Modal from "./Modal.jsx";
+import { useLanguage } from "@/context";
 
-function BaseModal({ open, onClose, className = '', children }) {
-  if (!open) return null
+function BaseModal({ open, onClose, className = "", children, closeLabel }) {
+  const { t } = useLanguage();
+  if (!open) return null;
+
+  const resolvedCloseLabel = closeLabel ?? t.close;
 
   return (
-    <Modal onClose={onClose} className={className}>
+    <Modal
+      onClose={onClose}
+      className={className}
+      closeLabel={resolvedCloseLabel}
+    >
       {children}
     </Modal>
-  )
+  );
 }
 
-export default BaseModal
+BaseModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  closeLabel: PropTypes.string,
+};
+
+export default BaseModal;
