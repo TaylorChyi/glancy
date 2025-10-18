5ff80f16247d8388af252496f3cd8b19c3e1f193
Tianyue Qi <taylor_chyi@icloud.com>
2025-10-18 22:37:34 +0800

    refactor: modularize profile page model

--- website/config/quality/eslint.config.js
+++ website/config/quality/eslint.config.js
@@ -60,7 +60,6 @@ const STRUCTURAL_DEBT_ALLOWLIST = [
   "src/app/pages/preferences/sections/historyExportSerializer.js",
   "src/app/pages/preferences/sections/subscriptionBlueprint.js",
   "src/app/pages/preferences/usePreferenceSections.js",
-  "src/app/pages/profile/index.jsx",
   "src/core/i18n/common/en.js",
   "src/core/i18n/common/zh.js",
   "src/core/store/history/historyStoreMachine.ts",
