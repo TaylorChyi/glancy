2272d3b2265517b9caedcfcc3579682c9c185408
Tianyue Qi <taylor_chyi@icloud.com>
2025-11-12 01:16:01 +0800

    clean code

--- backend/src/main/java/com/glancy/backend/service/shortcut/KeyboardShortcutService.java
+++ backend/src/main/java/com/glancy/backend/service/shortcut/KeyboardShortcutService.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
