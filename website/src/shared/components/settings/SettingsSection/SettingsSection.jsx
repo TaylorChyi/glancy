ce608109bccb75c4e9d009f7ed83f0cd4d6eb854
Tianyue Qi <taylor_chyi@icloud.com>
2025-11-14 02:59:50 +0800

    Refactor settings section composition

--- website/src/shared/components/settings/SettingsSection/SettingsSection.jsx
+++ website/src/shared/components/settings/SettingsSection/SettingsSection.jsx
@@ -6,6 +6,82 @@ const sanitizeId = (id) => id?.replaceAll(":", "-");
 const isRenderable = (value) =>
   value !== null && value !== undefined && value !== "";
 
+const resolveAriaDescribedBy = ({
+  describedBy,
+  ariaDescribedByFromProps,
+  shouldRenderDescription,
+  resolvedDescriptionId,
+}) =>
+  describedBy ??
+  ariaDescribedByFromProps ??
+  (shouldRenderDescription ? resolvedDescriptionId : undefined);
+
+const getSectionClasses = (classes = {}) => ({
+  sectionClassName: classes.section,
+  headerClassName: classes.header,
+  titleClassName: classes.title,
+  dividerClassName: classes.divider,
+  descriptionClassName: classes.description,
+});
+
+const splitSectionProps = (sectionProps) => {
+  const { "aria-describedby": ariaDescribedByFromProps, ...restSectionProps } =
+    sectionProps;
+  return { ariaDescribedByFromProps, restSectionProps };
+};
+
+const useResolvedDescriptionId = ({
+  shouldRenderDescription,
+  descriptionId,
+  headingId,
+  autoDescriptionId,
+}) =>
+  useMemo(() => {
+    if (!shouldRenderDescription) {
+      return undefined;
+    }
+    const candidateId =
+      descriptionId ?? `${headingId}-${sanitizeId(autoDescriptionId)}`;
+    return sanitizeId(candidateId);
+  }, [autoDescriptionId, descriptionId, headingId, shouldRenderDescription]);
+
+function SectionHeader({
+  headingId,
+  title,
+  showDivider,
+  headerClassName,
+  titleClassName,
+  dividerClassName,
+}) {
+  return (
+    <div className={headerClassName}>
+      <h3 id={headingId} className={titleClassName} tabIndex={-1}>
+        {title}
+      </h3>
+      {showDivider ? (
+        <div className={dividerClassName} aria-hidden="true" />
+      ) : null}
+    </div>
+  );
+}
+
+function SectionDescription({
+  shouldRender,
+  id,
+  className,
+  children,
+}) {
+  if (!shouldRender) {
+    return null;
+  }
+
+  return (
+    <p id={id} className={className}>
+      {children}
+    </p>
+  );
+}
+
 function SettingsSection({
   headingId,
   title,
@@ -19,55 +95,36 @@ function SettingsSection({
 }) {
   const autoDescriptionId = useId();
   const shouldRenderDescription = isRenderable(description);
-
-  const resolvedDescriptionId = useMemo(() => {
-    if (!shouldRenderDescription) {
-      return undefined;
-    }
-    const candidateId =
-      descriptionId ?? `${headingId}-${sanitizeId(autoDescriptionId)}`;
-    return sanitizeId(candidateId);
-  }, [autoDescriptionId, descriptionId, headingId, shouldRenderDescription]);
-
-  const {
-    section: sectionClassName,
-    header: headerClassName,
-    title: titleClassName,
-    divider: dividerClassName,
-    description: descriptionClassName,
-  } = classes;
-
-  const { "aria-describedby": ariaDescribedByFromProps, ...restSectionProps } =
-    sectionProps;
-  const ariaDescribedBy =
-    describedBy ??
-    ariaDescribedByFromProps ??
-    (shouldRenderDescription ? resolvedDescriptionId : undefined);
-
+  const resolvedDescriptionId = useResolvedDescriptionId({ shouldRenderDescription, descriptionId, headingId, autoDescriptionId });
+  const { sectionClassName, headerClassName, titleClassName, dividerClassName, descriptionClassName } = getSectionClasses(classes);
+  const { ariaDescribedByFromProps, restSectionProps } = splitSectionProps(sectionProps);
+  const ariaDescribedBy = resolveAriaDescribedBy({ describedBy, ariaDescribedByFromProps, shouldRenderDescription, resolvedDescriptionId });
+  const headerProps = { headingId, title, showDivider, headerClassName, titleClassName, dividerClassName };
+  const descriptionProps = { shouldRender: shouldRenderDescription, id: resolvedDescriptionId, className: descriptionClassName };
   return (
-    <section
-      aria-labelledby={headingId}
-      {...restSectionProps}
-      className={sectionClassName}
-      aria-describedby={ariaDescribedBy}
-    >
-      <div className={headerClassName}>
-        <h3 id={headingId} className={titleClassName} tabIndex={-1}>
-          {title}
-        </h3>
-        {showDivider ? (
-          <div className={dividerClassName} aria-hidden="true" />
-        ) : null}
-      </div>
-      {shouldRenderDescription ? (
-        <p id={resolvedDescriptionId} className={descriptionClassName}>
-          {description}
-        </p>
-      ) : null}
+    <section aria-labelledby={headingId} {...restSectionProps} className={sectionClassName} aria-describedby={ariaDescribedBy}>
+      <SectionHeader {...headerProps} />
+      <SectionDescription {...descriptionProps}>{description}</SectionDescription>
       {children}
     </section>
   );
 }
+
+SectionHeader.propTypes = {
+  headingId: PropTypes.string.isRequired,
+  title: PropTypes.node.isRequired,
+  showDivider: PropTypes.bool.isRequired,
+  headerClassName: PropTypes.string,
+  titleClassName: PropTypes.string,
+  dividerClassName: PropTypes.string,
+};
+
+SectionDescription.propTypes = {
+  shouldRender: PropTypes.bool.isRequired,
+  id: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
 
 SettingsSection.propTypes = {
   headingId: PropTypes.string.isRequired,
