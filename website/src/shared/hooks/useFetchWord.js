a4d54a7e76f81c37498694b1cbb917239cc73580
Tianyue Qi <taylor_chyi@icloud.com>
2025-11-15 02:43:23 +0800

    Refactor fetch word hook helpers

--- website/src/shared/hooks/useFetchWord.js
+++ website/src/shared/hooks/useFetchWord.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useApi } from "@shared/hooks/useApi.js";
 import {
   resolveWordLanguage,
@@ -6,49 +7,64 @@ import {
 } from "@shared/utils";
 import { DEFAULT_MODEL } from "@core/config";
 
+function createFetchWordParams({
+  user,
+  term,
+  model = DEFAULT_MODEL,
+  language = WORD_LANGUAGE_AUTO,
+  flavor = WORD_FLAVOR_BILINGUAL,
+  forceNew = false,
+  versionId,
+  captureHistory,
+}) {
+  const resolvedLanguage = resolveWordLanguage(term, language);
+  const resolvedFlavor = flavor ?? WORD_FLAVOR_BILINGUAL;
+
+  return {
+    request: {
+      userId: user.id,
+      term,
+      language: resolvedLanguage,
+      flavor: resolvedFlavor,
+      model,
+      token: user.token,
+      forceNew,
+      versionId,
+      captureHistory,
+    },
+    meta: {
+      language: resolvedLanguage,
+      flavor: resolvedFlavor,
+    },
+  };
+}
+
+function mapWordSuccess(data, meta) {
+  return { data, error: null, ...meta };
+}
+
+function mapWordFailure(error, meta) {
+  return { data: null, error, ...meta };
+}
+
 export function useFetchWord() {
   const api = useApi();
   const { fetchWord } = api.words;
 
-  const fetchWordWithHandling = async ({
-    user,
-    term,
-    model = DEFAULT_MODEL,
-    language = WORD_LANGUAGE_AUTO,
-    flavor = WORD_FLAVOR_BILINGUAL,
-    forceNew = false,
-    versionId,
-    captureHistory,
-  }) => {
-    const resolvedLanguage = resolveWordLanguage(term, language);
-    const resolvedFlavor = flavor ?? WORD_FLAVOR_BILINGUAL;
-    try {
-      const data = await fetchWord({
-        userId: user.id,
-        term,
-        language: resolvedLanguage,
-        flavor: resolvedFlavor,
-        model,
-        token: user.token,
-        forceNew,
-        versionId,
-        captureHistory,
-      });
-      return {
-        data,
-        error: null,
-        language: resolvedLanguage,
-        flavor: resolvedFlavor,
-      };
-    } catch (error) {
-      return {
-        data: null,
-        error,
-        language: resolvedLanguage,
-        flavor: resolvedFlavor,
-      };
-    }
-  };
+  const fetchWordWithHandling = useMemo(
+    () =>
+      async (options) => {
+        const { request, meta } = createFetchWordParams(options);
+
+        try {
+          const data = await fetchWord(request);
+          return mapWordSuccess(data, meta);
+        } catch (error) {
+          return mapWordFailure(error, meta);
+        }
+      },
+    [fetchWord],
+  );
 
   return { fetchWordWithHandling };
 }
